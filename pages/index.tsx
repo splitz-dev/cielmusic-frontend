d717e45a1411d4ffc8c4804d972d21a9c924063d
DDark <admin@ddark.kr>
2019-12-25 00:36:26 +0900

    add: TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div>
+      <p>테스트</p>
+    </div>
+  </>
+)
+
+export default Home
