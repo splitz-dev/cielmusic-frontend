1482d1e8a8301f131d7bbb5ec992a5d0d3f5ec9b
Dohyun Jung (DDark) <ddark.kr@gmail.com>
2019-12-27 08:59:24 +0900

    edit: Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 interface Props {}
+
+const SidebarElement = styled.aside`
+  background: var(--gray-700);
+`
 
 const Sidebar: React.SFC<Props> = () => {
   return (
     <>
-      <div></div>
+      <SidebarElement></SidebarElement>
     </>
   )
 }
