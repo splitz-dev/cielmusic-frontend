ad7855ae1c4d3b0f89962d1d500e7f183a247655
DDark <ddark.kr@gmail.com>
2019-12-31 22:21:18 +0900

    add: Button

---
+++ components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const ButtonElement = styled.button``
+
+const Button: React.FC = () => {
+  return (
+    <>
+      <ButtonElement></ButtonElement>
+    </>
+  )
+}
+
+export default Button
--- components/Layout/Part/Search.tsx
+++ components/Layout/Part/Search.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from '@emotion/styled'
-
 const SearchFormStyle = styled.form`
   border-radius: 100px;
   width: 400px;
