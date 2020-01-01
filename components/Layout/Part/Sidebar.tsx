dfd9db355ba4c7572cd6d9b2407712596cd6bbe9
DDark <ddark.kr@gmail.com>
2020-01-01 23:56:40 +0900

    edit: 메뉴

--- components/Layout/Part/Sidebar.tsx
+++ components/Layout/Part/Sidebar.tsx
@@ -12,18 +12,37 @@ const SidebarElement = styled.aside`
     &Logo {
       height: 55px;
       display: flex;
-      justify-content: center;
       align-items: center;
       font-weight: 700;
       font-size: 18px;
       text-decoration: none;
       color: #000;
+      padding: 0 24px;
       .svgElement {
         width: 25px;
         margin-right: 8px;
       }
       span {
         margin-top: -1px;
+      }
+    }
+  }
+  .Menu {
+    padding: 10px 0;
+    a {
+      height: 30px;
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      color: #000;
+      font-size: 0.88rem;
+      padding: 20px 24px;
+      margin: 5px 0;
+      svg {
+        color: #333;
+        width: 21px;
+        height: 30px;
+        margin-right: 12px;
       }
     }
   }
@@ -38,7 +57,59 @@ const Sidebar: React.FC = () => {
           <span>Ciel Music</span>
         </a>
       </Link>
-      <div className=""></div>
+      <div className="Menu">
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+          </svg>
+          홈
+        </a>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="8" y1="6" x2="21" y2="6"></line>
+            <line x1="8" y1="12" x2="21" y2="12"></line>
+            <line x1="8" y1="18" x2="21" y2="18"></line>
+            <line x1="3" y1="6" x2="3.01" y2="6"></line>
+            <line x1="3" y1="12" x2="3.01" y2="12"></line>
+            <line x1="3" y1="18" x2="3.01" y2="18"></line>
+          </svg>
+          랭킹
+        </a>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="3" y="3" width="7" height="7"></rect>
+            <rect x="14" y="3" width="7" height="7"></rect>
+            <rect x="14" y="14" width="7" height="7"></rect>
+            <rect x="3" y="14" width="7" height="7"></rect>
+          </svg>
+          내 재생목록
+        </a>
+      </div>
     </SidebarElement>
   )
 }
