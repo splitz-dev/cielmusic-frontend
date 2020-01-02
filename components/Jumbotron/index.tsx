e8f1900e8cab23b08e44752f897890821f7a6f83
Dohyun Jung (DDark) <ddark.kr@gmail.com>
2020-01-02 10:13:40 +0900

    edit: Jumbotron

--- components/Jumbotron/index.tsx
+++ components/Jumbotron/index.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styled from '@emotion/styled'
 
 const JumbStyle = styled.section`
-  background: linear-gradient(142deg, var(--blue-1) 0%, var(--blue-2) 100%);
-  padding: 20px 30px;
+  background: linear-gradient(10deg, var(--red) 0%, var(--yellow) 100%);
+  padding: 20px 35px;
   width: 100%;
   border-radius: 22px;
+  color: #fff;
 `
 
 const Jumbotron: React.FC = props => {
