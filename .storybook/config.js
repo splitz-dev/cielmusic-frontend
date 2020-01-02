821a03b35d2d4789e89e328c218a0750a8e62367
Dohyun Jung (DDark) <ddark.kr@gmail.com>
2020-01-02 09:16:00 +0900

    fixed ⚡ Storybook is working!! 😋

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
 import {
   configure
 } from '@storybook/react';
+
+import '!style-loader!css-loader!sass-loader!../styles/core.scss';
+
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../stories', true, /\.stories\.tsx?$/), module);
