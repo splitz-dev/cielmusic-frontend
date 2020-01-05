b833b7f9d779b63f53cb46b7ef98491549bb4ed3
kang-heewon <itphantom@icloud.com>
2020-01-06 00:27:38 +0900

    feat: ⚡️css minify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withSass = require('@zeit/next-sass')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = withSass({
   poweredByHeader: false,
+  webpack(config) {
+    config.optimization.minimizer = []
+    config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}))
+    return config
+  },
 })
