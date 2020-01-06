29ba9f291b8bf6233e3318e2959fbc52b336eb14
Dohyun Jung (DDark) <ddark.kr@gmail.com>
2020-01-06 09:12:58 +0900

    refactor: 🛠 Login 디렉터리 변경

--- components/PageComponent/Login/index.tsx
+++ components/PageComponent/Login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
-import LoginForm from '../LoginForm'
+import LoginForm from '../../LoginForm'
 
 const MainController = styled.main`
   background-image: linear-gradient(#f95d68, #fa964d), linear-gradient(101deg, #f95d68, #fa964d);
