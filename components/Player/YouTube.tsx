ef95d5a14c7aa3ceec1aacae52642e32e5ef0155
DDark <ddark.kr@gmail.com>
2020-01-09 00:54:43 +0900

    feature: ⚡️ YouTube player

---
+++ components/Player/YouTube.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import YouTube from 'react-youtube'
+
+const youtubePlayer = () => {
+  return (
+    <>
+      <YouTube />
+    </>
+  )
+}
+
+export default youtubePlayer
