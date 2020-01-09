67cb2495ffd6fa441c392fb49c0128b23e76a25e
kang-heewon <itphantom@icloud.com>
2020-01-09 23:35:28 +0900

    fix: 🔥 fix redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import App, { AppContext, AppInitialProps } from 'next/app'
 import React from 'react'
 import { Provider } from 'react-redux'
-import withRedux from 'next-redux-wrapper'
 import withReduxSaga from 'next-redux-saga'
 import '../styles/core.scss'
-import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer, { rootSaga } from '../modules'
+import { createStore, applyMiddleware } from 'redux'
 
 const sagaMiddleware = createSagaMiddleware()
+
 const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
 sagaMiddleware.run(rootSaga)
-
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps> {
     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
@@ -28,4 +27,4 @@ class MyApp extends App {
   }
 }
 
-export default withRedux(createStore)(withReduxSaga(MyApp))
+export default withReduxSaga(MyApp)
