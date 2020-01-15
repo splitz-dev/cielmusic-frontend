fe931b86fbfc5b5ceccd08ec39c0770cfefaa33a
kang-heewon <itphantom@icloud.com>
2020-01-15 17:46:41 +0900

    fix: 🔥 naming change

--- modules/login/actions.ts
+++ modules/login/actions.ts
@@ -2,9 +2,9 @@ import { createAsyncAction } from 'typesafe-actions'
 import { LoginResult, LoginParams } from '../../api/login'
 import { AxiosError } from 'axios'
 
-export const LOGIN_START = 'github/LOGIN_START'
-export const LOGIN_SUCCESS = 'github/LOGIN_SUCCESS'
-export const LOGIN_FAIL = 'github/LOGIN_FAIL'
+export const LOGIN_START = 'music/LOGIN_START'
+export const LOGIN_SUCCESS = 'music/LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'music/LOGIN_FAIL'
 
 export const loginAsync = createAsyncAction(LOGIN_START, LOGIN_SUCCESS, LOGIN_FAIL)<
   LoginParams,
