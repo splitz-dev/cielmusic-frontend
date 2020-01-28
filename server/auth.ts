9b94737c09bdf92a7a32cb9b69d7a6b470a4b093
DDark <ddark.kr@gmail.com>
2020-01-28 23:26:45 +0900

    feat: ⚡️ Express 로그인 로직? 구현중

--- server/auth.ts
+++ server/auth.ts
@@ -1,10 +1,20 @@
 import express from 'express'
+import { API_HOST } from './index'
+import axios from 'axios'
 
 const router = express.Router()
 
-router.get(
-  '/login',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {},
-)
+// 로그인
+router.get('/login', (req: express.Request, res: express.Response) => {
+  // 백-엔드로 로그인 리퀘스트
+  axios
+    .get(API_HOST + '/auth/login')
+    .then(data => {
+      // 로그인 성공
+    })
+    .catch(err => {
+      res.status(err.response.status)
+    })
+})
 
 export default router
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,8 @@ import next from 'next'
 // Router
 import authRouter from './auth'
 
+export const API_HOST: string = process.env.API_HOST || 'https://heewon.music.dev'
+
 const port: number = Number(process.env.PORT) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({
